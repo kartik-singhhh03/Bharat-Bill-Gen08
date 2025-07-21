363628c6863d1113e4009210cfb0aa4658931d94
Kartik singh <161111266+kartik-singhhh03@users.noreply.github.com>
2025-07-21 11:38:10 +0530

    Updated package-lock.json

---
+++ src/components/EnhancedInvoiceForm.tsx
@@ -0,0 +1,8 @@
+@@ .. @@
+                        reader.onload = (event) => {
+                          setInvoice(prev => ({
+                            ...prev,
+-                            companyLogo: event.target?.result as string
++                            companyLogo: event.target?.result as string
+                          }))
+                        }
