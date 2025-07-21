a6c5cf1e6f6a73115cd596418f1ab1cf5e1dc32d
Kartik singh <161111266+kartik-singhhh03@users.noreply.github.com>
2025-07-21 11:39:24 +0530

    Remove invalid diff markers from EnhancedInvoiceForm.tsx

--- src/components/EnhancedInvoiceForm.tsx
+++ src/components/EnhancedInvoiceForm.tsx
@@ -1,7 +1,6 @@
                         reader.onload = (event) => {
                           setInvoice(prev => ({
                             ...prev,
--                            companyLogo: event.target?.result as string
-+                            companyLogo: event.target?.result as string
+                            companyLogo: event.target?.result as string
                           }))
                         }
